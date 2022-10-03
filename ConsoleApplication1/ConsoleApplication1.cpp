27492a93069b77db1e1ccebf7cb765f0a1f272b0
Rodion R <65biso2@gmail.com>
2022-10-03 20:38:55 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/* 18.Описание главной функции, 
+ * описание текстовой строки, 
+ * оператор ввода и вывода строки, 
+ * вычисление длины строки;
+ */
+
+#define mainFunc int main()
+#define textString(name) string name
+#define printString(strName) cout<<strName
+#define readString(strName) cin>>strName
+#define getStringLength(strName) strName.length();
+
+mainFunc{
+	textString(myStr);
+	readString(myStr);
+	printString(myStr);
+	cout << endl;
+	printString(getStringLength(myStr));
+}
